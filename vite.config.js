1cf046afecd3a26f60c63d024be978df3d9eceea
Brandon <you@example.com>
2025-05-13 23:11:38 -0600

    Permitir host de Railway en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 export default {
   server: {
-    port: process.env.PORT || 3000,  // Escucha en el puerto proporcionado por Railway o en el puerto 3000
+    port: process.env.PORT || 3000,
     host: '0.0.0.0',  // Asegúrate de que sea accesible desde fuera de la máquina local
   },
   preview: {
     port: process.env.PORT || 3000,  // Usa el mismo puerto para la vista previa
     host: '0.0.0.0',  // Asegúrate de que sea accesible desde fuera de la máquina local
-  }
+    allowedHosts: ['juego21-production.up.railway.app'], // Permite el dominio de Railway
+  },
 }
