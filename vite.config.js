977a656963cfd25b75afe02e8e460fde95ac546b
Brandon <you@example.com>
2025-05-13 23:05:27 -0600

    Configuración para puerto y host en Railway

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 export default {
   server: {
-    proxy: {
-      '/api': 'http://localhost:5000'
-    },
-    allowedHosts: [
-      'juego21-production.up.railway.app'
-    ]
+    port: process.env.PORT || 3000,  // Escucha en el puerto proporcionado por Railway o en el puerto 3000
+    host: '0.0.0.0',  // Asegúrate de que sea accesible desde fuera de la máquina local
+  },
+  preview: {
+    port: process.env.PORT || 3000,  // Usa el mismo puerto para la vista previa
+    host: '0.0.0.0',  // Asegúrate de que sea accesible desde fuera de la máquina local
   }
-};
+}
